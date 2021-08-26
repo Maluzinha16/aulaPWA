38863e9780dab9ef6eb9950c31a2f11d874852d5
marianogueira <61321986+Maluzinha16@users.noreply.github.com>
2021-08-26 13:33:25 +0000

    codigo

---
+++ sw.js
@@ -0,0 +1,20 @@
+var cacheName = 'NomeApp-v';
+var filesToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+self.addEventListener('lnstal', function(e)){
+  e,waitUntil(
+    caches.open(cacheName).then(function(cache){
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(
+    cache.match(e.request).then(function(response){
+      return response || fetch(e.request);
+    })
+  );
+});
