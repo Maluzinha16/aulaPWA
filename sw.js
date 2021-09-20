38b64eda385cad2bab9f7b4a08d6f35b696844f8
marianogueira <61321986+Maluzinha16@users.noreply.github.com>
2021-09-20 11:48:55 +0000

    atualizacao

--- sw.js
+++ sw.js
@@ -1,6 +1,13 @@
 var cacheName = 'NomeApp-v';
 var filesToCache = [
-  './',
+  './aranhaindex.html',
+  './login.html',
+  './cssfaleconosco.htnl',
+  './estilo.css',
+  './estiloaranha.css',
+  './estilofale.css',
+  './estiloindex.css',
+  './imagem',
   './index.html',
   './manifest.json'
 ];
