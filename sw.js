d568887d133077779918d2be11c34e0c5934f1da
marianogueira <61321986+Maluzinha16@users.noreply.github.com>
2021-09-21 13:49:44 +0000

    Atualização

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 var cacheName = 'NomeApp-v';
 var filesToCache = [
-  './aranhaindex.html',
+    './',
+    './aranhaindex.html',
   './login.html',
   './cssfaleconosco.htnl',
   './estilo.css',
@@ -11,16 +12,16 @@ var filesToCache = [
   './index.html',
   './manifest.json'
 ];
-self.addEventListener('lnstal', function(e) {
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache){
+    caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
-self.addEventListener('fetch',function(e){
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(response){
+    caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
   );
